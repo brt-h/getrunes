d8cb43e4de17af59ab85c9575637d477ca92f4e0
TK <teaseungkim@gmail.com>
2018-10-24 18:26:36 -0700

    added css to rune table

--- src/components/RunesContainer.js
+++ src/components/RunesContainer.js
@@ -4,7 +4,8 @@ class RunesContainer extends Component {
   render() {
     return (
       <div className="runeTable">
-        <div className="runeRow">1 Rune Gold: 40 + {this.props.goldGiven} = {this.props.goldGiven + 40}</div>
+
+        <div className="runeRow">1 Rune Gold: 40 + {this.props.goldGiven} = {(this.props.goldGiven + 40)}</div>
         <div className="runeRow">2 Rune Gold: 80 + {this.props.goldGiven * 2} = {(this.props.goldGiven + 40) * 2}</div>
         <div className="runeRow">3 Rune Gold: 120 + {this.props.goldGiven * 3} = {(this.props.goldGiven + 40) * 3}</div>
         <div className="runeRow">4 Rune Gold: 160 + {this.props.goldGiven * 4} = {(this.props.goldGiven + 40) * 4}</div>
