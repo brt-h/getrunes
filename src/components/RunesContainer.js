0c711a0f1c904210d16cca95f1ea8693ee46b6dd
TK <teaseungkim@gmail.com>
2018-10-24 23:36:03 -0700

    added css to table

--- src/components/RunesContainer.js
+++ src/components/RunesContainer.js
@@ -8,23 +8,23 @@ class RunesContainer extends Component {
         <div className="runeTable">
           <div className="runeCol">
             <div className="runeTitle"># of Runes</div>
-            <div className="runeRow">1</div>
+            <div className="runeRow odd">1</div>
             <div className="runeRow">2</div>
-            <div className="runeRow">3</div>
+            <div className="runeRow odd">3</div>
             <div className="runeRow">4</div>
           </div>
           <div className="runeCol">
             <div className="runeTitle">Gold Per Player</div>
-            <div className="runeRow">{(this.props.goldGiven + 40) * 1}</div>
+            <div className="runeRow odd">{(this.props.goldGiven + 40) * 1}</div>
             <div className="runeRow">{(this.props.goldGiven + 40) * 2}</div>
-            <div className="runeRow">{(this.props.goldGiven + 40) * 3}</div>
+            <div className="runeRow odd">{(this.props.goldGiven + 40) * 3}</div>
             <div className="runeRow">{(this.props.goldGiven + 40) * 4}</div>
           </div>
           <div className="runeCol">
             <div className="runeTitle">Gold Per Team</div>
-            <div className="runeRow">{((this.props.goldGiven + 40) * 1) * 5}</div>
+            <div className="runeRow odd">{((this.props.goldGiven + 40) * 1) * 5}</div>
             <div className="runeRow">{((this.props.goldGiven + 40) * 2) * 5}</div>
-            <div className="runeRow">{((this.props.goldGiven + 40) * 3) * 5}</div>
+            <div className="runeRow odd">{((this.props.goldGiven + 40) * 3) * 5}</div>
             <div className="runeRow">{((this.props.goldGiven + 40) * 4) * 5}</div>
           </div>
         </div>
