e7d80a20250d45e1ac5b1b62ddf8e39e911e3ec0
Rob H <snipersremix@users.noreply.github.com>
2020-06-08 20:40:50 -0400

    Update ButtonContainer.js
    Fixed warnings for:   
      Line 8:   img elements must have an alt prop, either with meaningful text, or an empty string for decorative images  jsx-a11y/alt-text
      Line 13:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images  jsx-a11y/alt-text
      Line 21:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images  jsx-a11y/alt-text

--- src/components/ButtonContainer.js
+++ src/components/ButtonContainer.js
@@ -5,12 +5,12 @@ class ButtonContainer extends Component {
     let button;
     if (this.props.started) {
       button = <div className="Button PauseB" onClick={this.props.handleClickPause}>
-      <img className="twitchIco" src={"https://static-cdn.jtvnw.net/emoticons/v1/65/1.0"}/>
+      <img className="twitchIco" src={"https://static-cdn.jtvnw.net/emoticons/v1/65/1.0"} alt="FrankerZ"/>
          Pause
       </div>;
     } else {
       button = <div className="Button StartB" onClick={this.props.handleClickStart}>
-      <img className="twitchIco" src={"https://static-cdn.jtvnw.net/emoticons/v1/81248/1.0"}/>
+      <img className="twitchIco" src={"https://static-cdn.jtvnw.net/emoticons/v1/81248/1.0"} alt="OSFrog"/>
          Start
       </div>;
     }
@@ -18,7 +18,7 @@ class ButtonContainer extends Component {
       <div className="ButtonContainer">
         {button}
         <div className="Button StopB" onClick={this.props.handleClickStop}>
-        <img className="twitchIco" src={"https://static-cdn.jtvnw.net/emoticons/v1/22639/1.0"}/>
+        <img className="twitchIco" src={"https://static-cdn.jtvnw.net/emoticons/v1/22639/1.0"} alt="BabyRage"/>
           Stop
         </div>
       </div>
